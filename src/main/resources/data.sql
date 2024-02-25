9f39c72b1bd679c90953d2870b8d0b0a11606b64
Your Name <you@example.com>
2024-02-25 12:59:14 +0500

    finalCommit

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO task(description, status, created_at) VALUES('Первая задача', 'NOT_STARTED', CURRENT_TIMESTAMP);
-INSERT INTO task(description, status, created_at) VALUES('Вторая задача', 'IN_PROGRESS', CURRENT_TIMESTAMP);
-INSERT INTO task(description, status, created_at) VALUES('Третья задача', 'COMPLETED', CURRENT_TIMESTAMP);
--- src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE task (
-    id SERIAL PRIMARY KEY,
-    description VARCHAR(255) NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL
-);
